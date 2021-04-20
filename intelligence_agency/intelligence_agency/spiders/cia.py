940b6e7b77664345f0ddad1840b71fa51a5a37b9
Jorge Nuñez <georgenul@live.com>
2021-04-20 11:38:39 -0500

    project finished

--- intelligence_agency/intelligence_agency/spiders/cia.py
+++ intelligence_agency/intelligence_agency/spiders/cia.py
@@ -11,5 +11,21 @@ class spider_cia(scrapy.Spider):
         'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
-    parse(self, response):
+    def parse(self, response):
         links_declassified = response.xpath('//a[starts-with(@href, "collection") and (parent::h3|parent::h2)]/@href').getall()
+        for link in links_declassified:
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={
+                # urljoin() combina la url absoluta con las que extrajimos de la web:
+                'url': response.urljoin(link)
+            })
+
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath('//div[@class="field-item even"]//p[not(@class)]/text()').get()
+
+        yield {
+            'url': link,
+            'title': title,
+            'body': paragraph
+        }
